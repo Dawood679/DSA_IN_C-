cc361bc5d3147aefc1ce3bc440a791f38b61c47a
Dawood Alam <dawoodalam057@gmail.com>
2026-07-03 07:47:00 +0500

    Bubble sorting

---
+++ Buuble_Sorting.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main (){
+    int arr[] = {5,4,3,2,1};
+    int n = 5;
+
+
+    for(int i = n-1;i>=0;i--){
+        for(int j = 0 ; j <i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+
+    for(int i = 0 ; i < 5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0 ;
+}
