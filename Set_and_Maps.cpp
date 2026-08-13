2f30c30b75b8fd216c1de4a19c54eea72060fd18
Dawood Alam <dawoodalam057@gmail.com>
2026-08-13 08:54:50 +0500

    fix added

---
+++ Set_and_Maps.cpp
@@ -0,0 +1,117 @@
+// #include<iostream>
+
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,2,3,3,4,4,5,5};
+
+//     for(int i = 0 ; i < 8;i++){
+//         for(int j = i+1; j< 8;j++){
+//             if(arr[i] == arr[j]){
+//                 cout<<"Duplicate mil gaya"<<" "<< arr[i]<<arr[j];
+//                 break;
+//             }
+//         }
+//     }
+
+
+
+
+
+//     return 0;
+// }
+
+
+// frequncy in the array of elements
+// #include<iostream>
+
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,2,3,3,4,4,5,5};
+
+//     for(int i = 0 ; i < 8;i++){
+//         if(arr[i] == -1){
+//             continue;
+//         }
+//         int count = 1;
+//         for(int j = i+1; j< 8;j++){
+//             if(arr[i] == arr[j]){
+//                 count++;
+//                 arr[j] = -1;
+//             }
+//         }
+//         cout<<"Frquncy of:"<<arr[i]<<" "<<count<<endl;
+//     }
+
+
+
+
+
+//     return 0;
+// }
+
+
+
+// //Find duplicate in the arry with sets
+
+
+// #include<iostream>
+// #include <set>
+
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,2,3,3,4,4,5,5};
+
+//     set<int> use;
+//     for(int i = 0 ; i < 8;i++){
+//         if(use.contains(arr[i])){
+//             break;
+//         }
+//         use.insert(arr[i]);
+//     }
+    
+
+    
+
+//     return 0;
+// }
+
+
+//find the frequncy of the elements with maps
+
+
+#include<iostream>
+#include <map>
+
+using namespace std;
+
+
+
+int main(){
+    int arr[] = {1,2,3,3,4,4,5,5};
+
+    map<int,int> use;
+    for(int i = 0 ; i < 8;i++){
+        if(use.contains(arr[i])){
+            use[arr[i]]++;
+        }else{
+
+            use.insert({arr[i],1});
+        }
+    }
+    
+    for (auto [key, value] : use) {
+    cout << key << " -> " << value << endl;
+}
+    
+
+    return 0;
+}
