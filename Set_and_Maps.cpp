25e467a6be423527b9509fe0fa62f7163ace3f03
Dawood Alam <dawoodalam057@gmail.com>
2026-08-17 08:48:25 +0500

    set and maps new problems

--- Set_and_Maps.cpp
+++ Set_and_Maps.cpp
@@ -1,3 +1,7 @@
+// to run the file
+// g++ -std=c++20 Set_and_Maps.cpp -o app
+
+
 // #include<iostream>
 
 // using namespace std;
@@ -148,31 +152,98 @@
 
 //union of the two arrays
 
-#include<iostream>
-#include <set>
+// #include<iostream>
+// #include <set>
+
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,2,3,3,4,4,5,5};
+//     int arr2[] = {7,8,9,10,11};
+//     set<int> st;
+//     for(int i = 0 ; i< 8;i++){
+//         st.insert(arr[i]);
+//     }
+//    for(int i = 0 ; i< 5;i++){
+//         st.insert(arr2[i]);
+//     }
+
+
+
+//      for (int num : st) {
+//         std::cout << num << " ";
+//     }
+    
+    
+
+//     return 0;
+// }
+
+
+// // two sum problem with sets
+// #include<iostream>
+// #include <set>
+
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,4,5,6,7,3};
+//     int target = 9;
+//     set<int> st;
+//     for(int i = 0 ; i< 6;i++){
+//         int req  = target - arr[i];
+//         if(st.contains(req)){
+//             break;
+//         }
+//         st.insert(arr[i]);
+//     }
+   
+
+
+
+//      for (int num : st) {
+//         std::cout << num << " ";
+//     }
+    
+    
+
+//     return 0;
+// }
+
+
+
+
+// two sum problem with maps and also the count
+#include <iostream>
+#include <map>
 
 using namespace std;
 
+int main() {
+    int arr[] = {1, 4, 5, 6, 7, 3};
+    int target = 9;
 
+    int ans = 0;
+    map<int, int> mp;
 
-int main(){
-    int arr[] = {1,2,3,3,4,4,5,5};
-    int arr2[] = {7,8,9,10,11};
-    set<int> st;
-    for(int i = 0 ; i< 8;i++){
-        st.insert(arr[i]);
+    for (int i = 0; i < 6; i++) {
+
+        int req = target - arr[i];
+
+        // required number already exists?
+        if (mp.contains(req)) {
+            ans += mp[req];
+        }
+
+        // store current number
+        mp[arr[i]]++;
     }
-   for(int i = 0 ; i< 5;i++){
-        st.insert(arr2[i]);
-    }
 
-
-
-     for (int num : st) {
-        std::cout << num << " ";
-    }
-    
-    
+    cout << "Total pairs: " << ans << endl;
 
     return 0;
 }
