280e483df74f35a261a01497f45c767c44af3a52
Dawood Alam <dawoodalam057@gmail.com>
2026-08-27 08:08:17 +0500

    sliding widnows

--- Sliding_window_and_two_pointer.cpp
+++ Sliding_window_and_two_pointer.cpp
@@ -1,51 +1,106 @@
+// #include<iostream>
+// using namespace std;
+
+
+
+// int main(){
+//     // first apporoch the sovel th problem of sum of sub array
+//     for(int i = 0 ; i < 8;i++){
+//         for(int j = i; j< 8;j++){
+//             for(int k = i; k<=j;k++){
+//                 cout<<arr[k]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//     }
+
+//       // second apporoch the sovel th problem of sum of sub array
+//       // prefix sum method
+//       for(int i = 0 ; i < 8;i++){
+//         for(int j = i; j< 8;j++){
+//             int sum = 0;
+//             for(int k = i; k<=j;k++){
+//                 sum+=arr[k];
+//             }
+//             cout<<sum<<endl;
+//         }
+//     for(int i = 0 ; i < 8;i++){
+//         for(int j = i; j< 8;j++){
+//             // prefix sum method
+//             if(l == 0){
+//                 cout<<p[r]<<endl;
+//             }else{
+//                 cout<<p[r]-p[l-1]<<endl;    
+
+//             cout<<endl;
+//         }
+//     }
+
+
+
+//     // carry forward techinque
+//      for(int i = 0 ; i < 8;i++){
+//         int sum = 0;
+//         for(int j = i; j< 8;j++){
+//             sum += arr[j];
+//             cout<<sum<<endl;
+//         }
+//     }
+
+// }}
+
+
+
+// // carry forward technigque with sum of suba arry with size k = x
+
+// #include<iostream>
+// using namespace std;
+
+
+
+// int main(){
+//     int arr[] = {1,2,3,4,5};
+//     int ans =-32232232;
+//     int k = 3;
+//     for(int l = 0 ; l < 5;l++){
+//         int sum =0;
+//         for(int r = l ; r < 5;r++){
+//             sum += arr[r];
+//             int len = r-l+1;
+//             if(len == k){
+//                 ans = max(ans,sum);
+//             }
+//         }
+//     }
+//     cout<<ans;
+
+// }
+
+
+
+// carry forward technigque with sum of suba arry with size k = x with optimize solution
+
 #include<iostream>
 using namespace std;
 
 
 
 int main(){
-    // first apporoch the sovel th problem of sum of sub array
-    for(int i = 0 ; i < 8;i++){
-        for(int j = i; j< 8;j++){
-            for(int k = i; k<=j;k++){
-                cout<<arr[k]<<" ";
-            }
-            cout<<endl;
-        }
+    int arr[] = {1,2,3,4,5};
+    int ans =-32232232;
+    int k = 3;
+    int sum =0;
+    //create a sum for first wind
+    for(int i = 0 ; i< k;i++){
+        sum +=arr[i];
     }
 
-      // second apporoch the sovel th problem of sum of sub array
-      // prefix sum method
-      for(int i = 0 ; i < 8;i++){
-        for(int j = i; j< 8;j++){
-            int sum = 0;
-            for(int k = i; k<=j;k++){
-                sum+=arr[k];
-            }
-            cout<<sum<<endl;
-        }
-    for(int i = 0 ; i < 8;i++){
-        for(int j = i; j< 8;j++){
-            // prefix sum method
-            if(l == 0){
-                cout<<p[r]<<endl;
-            }else{
-                cout<<p[r]-p[l-1]<<endl;    
-
-            cout<<endl;
-        }
+    //build the sol for next window
+    for(int i = k ; i < 5;i++){
+        sum += arr[i];
+        sum -= arr[i-k];
+        ans = max(ans,sum);
     }
-
-
-
-    // carry forward techinque
-     for(int i = 0 ; i < 8;i++){
-        int sum = 0;
-        for(int j = i; j< 8;j++){
-            sum += arr[j];
-            cout<<sum<<endl;
-        }
-    }
-
-}}
+    cout<<ans;
+}
 
